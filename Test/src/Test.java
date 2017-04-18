279c3a9a33470c4148fdd51d59050312b40e53d5
tiwari2110 <akashtiwari2121@gmail.com>
2017-04-18 14:55:21 +0530

    First commit

---
+++ Test/src/Test.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  Scanner scan = new Scanner(System.in);
+	        int a = scan.nextInt();
+	        
+	        for(int i=1; i<=10; i++){
+	        	System.out.println(a + " x " + i + " = " + (a*i));
+	        }
+	       
+	      
+	}
+
+}
